b1693357dddebccf0ce19511715097116a82c382
Milan Bloem <milanbloem@gmail.com>
2020-11-22 12:20:44 +0100

    first buttons home screen

--- app/src/main/java/com/example/dobbelspel/WelcomeScreen.kt
+++ app/src/main/java/com/example/dobbelspel/WelcomeScreen.kt
@@ -3,7 +3,7 @@ package com.example.dobbelspel
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class WelcomeScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
