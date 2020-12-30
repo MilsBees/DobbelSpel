7d077051829525480f8c3dd5ab30b37bd3e24cd2
Milan Bloem <milanbloem@gmail.com>
2020-12-30 18:47:59 +0100

    Added new pages including transition to new game page

--- app/src/main/java/com/example/dobbelspel/WelcomeScreen.kt
+++ app/src/main/java/com/example/dobbelspel/WelcomeScreen.kt
@@ -1,11 +1,19 @@
 package com.example.dobbelspel
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class WelcomeScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnNewGame = findViewById<Button>(R.id.btnNewGame) as Button
+        btnNewGame.setOnClickListener {
+            startActivity(Intent(this@WelcomeScreen, GameActivity::class.java))
+        }
     }
 }
